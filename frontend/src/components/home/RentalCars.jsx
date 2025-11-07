e74abdef9fbad7641784daee47c17b00a2c37715
0786abu <abubakar77a9@gmail.com>
2025-11-07 21:23:12 +0500

    change

--- frontend/src/components/home/RentalCars.jsx
+++ frontend/src/components/home/RentalCars.jsx
@@ -43,7 +43,7 @@ const carModels = [
   "2011", "2012", "2013", "2014", "2015",
   "2016", "2017", "2018", "2019", "2020",
   "2021", "2022", "2023", "2024", "2025"]
-  
+
 const RentalCars = () => {
     const {cars,carloading} = useSelector((state)=>state.Car);
     const dispatch = useDispatch();
