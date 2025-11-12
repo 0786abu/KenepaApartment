99c486b17ee46481b4204ecb054e713c610ca17f
0786abu <abubakar77a9@gmail.com>
2025-11-12 22:30:29 +0500

    change

--- frontend/src/components/home/Hero_Section.jsx
+++ frontend/src/components/home/Hero_Section.jsx
@@ -2,7 +2,7 @@
 const Hero_Section = () => {
   return (
   <div>
-      <div className=' bg-[url(/images/hero.webp)] relative mt-16 bg-cover bg-no-repeat bg-center h-[80vh]'>
+      <div className=' bg-[url(/images/hero.webp)] relative mt-12 bg-cover bg-no-repeat bg-center h-[80vh]'>
     </div>
   </div>
   )
